ab5270f8492e5e469f1b5b6a40ed6e689275c22d
Qwame <32999739+iQwame@users.noreply.github.com>
2020-02-18 09:04:10 +0000

    Python
    py

---
+++ First.py
@@ -0,0 +1,17 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello, world")
+Hello, world
+>>> if 5 > 2:
+	print("Five is greater than two")
+
+	
+Five is greater than two
+>>> y = 4
+>>> x = "hello,world"
+>>> print(x)
+hello,world
+>>> print(y)
+4
+>>> #tried to cemment
+>>> 
